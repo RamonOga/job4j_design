4cca4cf15177ed733110420f88c112d6086add5b
Markelov Roman <roman.sercent@gmail.com>
2020-11-29 20:14:48 +0300

    1. Создать модель User [#407741]

--- chapter_001/src/main/java/ru/job4j/hashandequals/User.java
+++ chapter_001/src/main/java/ru/job4j/hashandequals/User.java
@@ -1,15 +1,46 @@
 package ru.job4j.hashandequals;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 public class User {
-    String name;
-    int children;
-    Calendar birthday;
+    private String name;
+    private int children;
+    private Calendar birthday;
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday
+                + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int rsl = Objects.hash(name, children, birthday);
+       //System.out.println(rsl);
+        return rsl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+                 && Objects.equals(name, user.name)
+                 && Objects.equals(birthday, user.birthday);
     }
 }
