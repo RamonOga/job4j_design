f58c6277060a00737dbc37e89303a9994508e2cc
Markelov Roman <roman.sercent@gmail.com>
2020-12-09 21:51:38 +0300

    generic test

--- chapter_001/src/main/java/ru/job4j/exam/Runner.java
+++ chapter_001/src/main/java/ru/job4j/exam/Runner.java
@@ -6,9 +6,17 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        User u1 = new User(1, "2");
-        User u2 = new User(1, "123");
-        System.out.println(u1.equals(u2));
+        List<? extends Number> list1 = new ArrayList<>();
+        List<? super Number> list2 = new ArrayList<>();
+        Number n = 12;
+        Object o = 12;
+        Integer i = 12;
+        list1.add(n);
+        list2.add(n);
+        list1.add(o);
+        list2.add(o);
+        list1.add(i);
+        list2.add(i);
     }
 }
 
