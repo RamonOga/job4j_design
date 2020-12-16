418862b016c63ff46f492b64ca7ce3f6cb4fb9ed
Markelov Roman <roman.sercent@gmail.com>
2020-12-16 12:16:57 +0300

    0.1. FileOutputStream. [#407761]ver 0.2

--- chapter_002/src/main/java/ru/job4j/io/FileOutStream.java
+++ chapter_002/src/main/java/ru/job4j/io/FileOutStream.java
@@ -4,14 +4,16 @@ import java.io.FileOutputStream;
 
 public class FileOutStream {
     public static void main(String[] args) {
-        String s = "";
+        StringBuilder s = new StringBuilder();
         for (int x = 1; x < 10; x++) {
             for (int y = 1; y < 10; y++) {
-                    s += ((x + " * " + y + " = " + (x * y)) + System.lineSeparator());
+                    s.append(x)
+                            .append(" * ").append(y).append(" = ")
+                            .append(x * y).append(System.lineSeparator());
             }
         }
         try (java.io.FileOutputStream out = new java.io.FileOutputStream("TableMultiplication.txt")) {
-            out.write(s.getBytes());
+            out.write(s.toString().getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
