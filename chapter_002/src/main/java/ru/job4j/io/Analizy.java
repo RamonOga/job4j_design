8a52ba441bcf53e20a8945ddcf7812402c49f3db
Markelov Roman <roman.sercent@gmail.com>
2020-12-21 22:41:12 +0300

    2. Анализ доступности сервера. [#407768]

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -8,39 +8,36 @@ import java.util.List;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<String> list = null;
+        List<String> list = new ArrayList<>();
         StringJoiner joiner = new StringJoiner("");
         int count = 0;
+        StringBuilder text = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            list = in.lines()
-                    .filter(a -> a.length() > 0)
-                    .collect(Collectors.toList());
+            String s = in.readLine();
+            while (s != null) {
+                if (count == 0 && (s.contains("400") || s.contains("500"))) {
+                    text.append(s.split(" ")[1]).append(";");
+                    count++;
+                }
+                if (count == 1  && (s.contains("200") || s.contains("300"))) {
+                    text.append(s.split(" ")[1]).append(System.lineSeparator());
+                    count = 0;
+                    list.add(text.toString());
+                    text.delete(0, text.length() - 1);
+                }
+                s = in.readLine();
+            }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        if (list != null) {
-            for (String s : list) {
-                if (count == 0 && (s.contains("400") || s.contains("500"))) {
-                    joiner.add(s.split(" ")[1] + ";");
-                    count++;
-                    continue;
-                }
-                if (count == 1 && (s.contains("200") || s.contains("300"))) {
-                    joiner.add(s.split(" ")[1] + System.lineSeparator());
-                    count = 0;
-                }
-            }
         }
         try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            out.write(joiner.toString());
+            for (String s : list) {
+                out.write(s);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-    }
-
-    public static void main(String[] args) {
-        Analizy anal = new Analizy();
-        anal.unavailable("./chapter_002/data/server.log", "./chapter_002/data/ShutdownsLog.log");
     }
 }
