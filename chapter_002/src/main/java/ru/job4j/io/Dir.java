247a2e4fbbb5bcd9082fdd4533956b6b4e5fdeed
Markelov Roman <roman.sercent@gmail.com>
2020-12-22 23:07:53 +0300

    4.0. File [#407764]

---
+++ chapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getAbsoluteFile());
+            System.out.println(String.format("size : %s", subfile.getTotalSpace()));
+        }
+    }
+}
