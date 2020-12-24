b64a77b3435eb4f44e15e569514ffd299d133092
Markelov Roman <roman.sercent@gmail.com>
2020-12-24 20:27:59 +0300

    4.1. Сканирование файловой системы. [#407758]

---
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        search(start, "js").forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PrimitiveIterator;
+import java.util.function.Predicate;
+
+public class SearchFiles implements FileVisitor<Path> {
+
+    private Predicate<Path> predicate;
+    private List<Path> list = new ArrayList<>();
+
+    public SearchFiles(Predicate<Path> pred) {
+        predicate = pred;
+    }
+
+    public List<Path> getPaths() {
+        return list;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (predicate.test(file)) {
+            list.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+}
