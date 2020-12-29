2b5dcb76b6d17e599693e8385cdfa8992d875c34
Markelov Roman <roman.sercent@gmail.com>
2020-12-29 18:59:18 +0300

    5. Валидация параметров запуска. [#407760]

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -8,8 +8,11 @@ import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, "js").forEach(System.out::println);
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Enter path and extension file");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, args[1]).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
