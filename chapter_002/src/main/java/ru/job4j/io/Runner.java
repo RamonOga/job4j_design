fa45c5e6866ab654c779d5679a13a50def8f7228
Markelov Roman <roman.sercent@gmail.com>
2021-01-08 23:00:23 +0300

    trash commit

--- chapter_002/src/main/java/ru/job4j/io/Runner.java
+++ chapter_002/src/main/java/ru/job4j/io/Runner.java
@@ -1,6 +1,14 @@
 package ru.job4j.io;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File f1 = new File("C:\\projects\\job4j_design\\chapter_002\\data\\test\\file 1.txt");
+        System.out.println(f1.toPath());
+        System.out.println(f1.toPath().endsWith("txt"));
+        System.out.println(f1.toPath().toString().endsWith("txt"));
     }
 }
