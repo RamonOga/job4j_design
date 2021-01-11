bde9b162550cd2361da24a63635e43b21e6b95e9
Markelov Roman <roman.sercent@gmail.com>
2021-01-11 20:19:48 +0300

    1. Log4j. Логирование системы. [#407753]

--- chapter_002/src/main/java/ru/job4j/io/logs/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/logs/UsageLog4j.java
@@ -1,11 +1,11 @@
 package ru.job4j.io.logs;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
 
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         LOG.trace("trace message");
