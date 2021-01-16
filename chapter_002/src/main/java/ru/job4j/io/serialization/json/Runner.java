e9e918be680066532635a99482124741d506fc7d
Markelov Roman <roman.sercent@gmail.com>
2021-01-16 12:59:25 +0300

    2. Формат JSON [#407774]

--- chapter_002/src/main/java/ru/job4j/io/serialization/json/Runner.java
+++ chapter_002/src/main/java/ru/job4j/io/serialization/json/Runner.java
@@ -3,28 +3,35 @@ package ru.job4j.io.serialization.json;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Runner {
     public static void main(String[] args) {
-        final Person person = new Person(false, 35, new Contact("+7-917-000-00-00"), "Programer", "Tester");
+       Driver driverOut = new Driver();
+       final Gson gson = new Gson().newBuilder().setPrettyPrinting().create();
+       String jsonOut = gson.toJson(driverOut);
 
-        /* Преобразуем объект person в json-строку. */
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(person));
+       try (BufferedWriter out = new BufferedWriter(new FileWriter("json_test.json"))) {
+           out.write(jsonOut);
+       } catch (IOException e) {
+           e.fillInStackTrace();
+       }
+       StringBuffer jsonIn = new StringBuffer();
+       try (BufferedReader in = new BufferedReader(new FileReader("json_test.json"))) {
+           String inString;
+           while ((inString = in.readLine()) != null) {
+               jsonIn.append(inString);
+           }
+        } catch (IOException ex) {
+           ex.fillInStackTrace();
+        }
+        Driver driverIn = gson.fromJson(jsonIn.toString(), Driver.class);
+        System.out.println(driverIn);
+        System.out.println(driverIn.equals(driverOut));
+        System.out.println(driverOut.equals(driverIn));
 
-        /* Модифицируем json-строку */
-        final String personJson =
-                "{"
-                        + "\"sex\":false,"
-                        + "\"age\":35,"
-                        + "\"contact\":"
-                        + "{"
-                        + "\"phone\":\"+7(924)111-111-11-11\""
-                        + "},"
-                        + "\"statuses\":"
-                        + "[\"Student\",\"Free\"]"
-                        + "}";
-        final Person personMod = gson.fromJson(personJson, Person.class);
-        System.out.println(personMod);
     }
 }
 
@@ -33,4 +40,36 @@ class Driver {
     String name = "Peduart";
     boolean adequate = false;
     String[] categories = new String[]{"B", "B1"};
+
+    @Override
+    public String toString() {
+        return "Driver{"
+                + "age=" + age
+                + ", name='" + name + '\''
+                + ", adequate=" + adequate
+                + ", categories=" + Arrays.toString(categories)
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Driver driver = (Driver) o;
+        return age == driver.age
+                && adequate == driver.adequate
+                && Objects.equals(name, driver.name)
+                && Arrays.equals(categories, driver.categories);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(age, name, adequate);
+        result = 31 * result + Arrays.hashCode(categories);
+        return result;
+    }
 }
