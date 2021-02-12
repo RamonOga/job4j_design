a6e4425c0aaed84a7c0f30f62445283b7825e7a8
Roman Markelov <Roman.sercent@gmail.com>
2021-02-12 11:17:59 +0300

    ShellPath

---
+++ chapter_002/src/main/java/ru/job4j/io/linuxsimulator/ShallPath.java
@@ -0,0 +1,25 @@
+package ru.job4j.io.linuxsimulator;
+
+import java.util.Stack;
+import java.util.StringJoiner;
+
+public class ShallPath {
+    Stack<String> stack = new Stack<>();
+
+    public void push(String input) {
+        stack.push(input);
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public String print() {
+        StringJoiner sj = new StringJoiner("/", "/", "");
+        for (String s : stack) {
+            sj.add(s);
+        }
+        System.out.println(sj);
+        return sj.toString();
+    }
+}
