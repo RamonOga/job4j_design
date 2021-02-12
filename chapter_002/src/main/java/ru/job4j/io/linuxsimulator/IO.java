d2a6c54c38b4906f8ad28d09228dee5eec8a7966
Roman Markelov <Roman.sercent@gmail.com>
2021-02-12 15:28:14 +0300

    ShellPath

--- chapter_002/src/main/java/ru/job4j/io/linuxsimulator/IO.java
+++ chapter_002/src/main/java/ru/job4j/io/linuxsimulator/IO.java
@@ -26,6 +26,8 @@ public class IO {
             String[] tmp = command.split(" ");
             if (tmp.length == 2) {
                 rsl = tmp;
+            } else {
+                rsl[0] = tmp[0];
             }
         }
         return rsl;
