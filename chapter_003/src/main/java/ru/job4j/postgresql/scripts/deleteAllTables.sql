23fae3a5227270c73e430bea0f4f6150fcf5ceee
Markelov Roman <roman.sercent@gmail.com>
2021-02-12 13:06:44 +0300

    1. Симуляция терминала Linux

---
+++ chapter_003/src/main/java/ru/job4j/postgresql/scripts/deleteAllTables.sql
@@ -0,0 +1,9 @@
+drop table rules_role cascade;
+drop table rules cascade;
+drop table role cascade;
+drop table item;
+drop table item_user;
+drop table category;
+drop table comments;
+drop table attaches;
+drop table state;
---
+++ chapter_003/src/main/java/ru/job4j/postgresql/scripts/deleteDB.sql
@@ -0,0 +1,1 @@
+drop database items_db;
