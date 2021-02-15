050b5ec4417bc3395398f265e96e144441d307a5
Markelov Roman <roman.sercent@gmail.com>
2021-02-15 20:46:15 +0300

    0. Видео [#407699]

--- chapter_003/src/main/java/ru/job4j/postgresql/scripts/leftrightjoin/create.sql
+++ chapter_003/src/main/java/ru/job4j/postgresql/scripts/leftrightjoin/create.sql
@@ -8,3 +8,9 @@ create table emploees (
         name varchar(255),
         depo_id int references deportments(id)
 );
+
+create table teens (
+  id serial primary key,
+  name varchar(255),
+  gender varchar(255)
+);
--- chapter_003/src/main/java/ru/job4j/postgresql/scripts/leftrightjoin/query.sql
+++ chapter_003/src/main/java/ru/job4j/postgresql/scripts/leftrightjoin/query.sql
@@ -1,3 +1,20 @@
-select d.name as Отдел, e.name as Работник from deporment d left join emploees e on d.id = e.depo_id;
-select d.name as Отдел, e.name as Работник from emploees e right join  deportments d on d.id = e.depo_id;
-select d.name as Отдел, e.name as Работник from emploees e right join  deportments d on d.id = e.depo_id where e.dep;
+-- Используя left и right join написать запросы, которые давали бы одинаковый результат.
+select d.name as Отдел, e.name as Работник
+from deportments d
+         left join emploees e on d.id = e.depo_id;
+
+select d.name as Отдел, e.name as Работник
+from emploees e
+         right join  deportments d on d.id = e.depo_id;
+
+-- Используя left join найти департаменты, у которых нет работников
+select d.name as Отдел, e.name as Работник
+from deportments d
+    left join emploees e on d.id = e.depo_id
+where e.depo_id is null;
+
+-- Создать таблицу teens с атрибутами name, gender и заполнить ее.
+-- Используя cross join составить все возможные разнополые пары
+select t1.name, t2.name from teens as t1
+                                 cross join teens as t2
+where t1.gender != t2.gender and t1.gender != 'Female';
