f0027fab48b8a67da85f915bd0f289bef2a2108a
Roman Markelov <Roman.sercent@gmail.com>
2021-02-16 10:53:02 +0300

    qqq

--- chapter_003/src/main/java/ru/job4j/postgresql/scripts/filters/query.sql
+++ chapter_003/src/main/java/ru/job4j/postgresql/scripts/filters/query.sql
@@ -6,16 +6,13 @@ select p.name, t.name from product as p join type as t on p.type_id = t.id and t
 select * from product where name like '%icecream%';
 
 -- 3. Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
-select * from product where expired_date < current_date + interval '1 month';
+select * from product where extract (month from expired_date) = extract (month from current_date) + 1;
 
 -- 4. Написать запрос, который выводит самый дорогой продукт.
 select * from product where price = (select max(price) from product);
 
 -- 5. Написать запрос, который выводит количество всех продуктов определенного типа.
-select t.name, sum(p.quantity) from type as t
-join product as p on p.type_id = t.id
-group by t.name, p.type_id, t.id
-having p.type_id = t.id;
+select t.name, sum(p.quantity) from type as t join product as p on p.type_id = t.id group by t.name, p.type_id, t.id having p.type_id = t.id;
 
 -- 6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
 select p.name, t.name, p.price, p.quantity, p.expired_date from product as p
@@ -23,7 +20,7 @@ select p.name, t.name, p.price, p.quantity, p.expired_date from product as p
           on p.type_id = t.id and (t.name = 'cheese' or t.name = 'milk');
 
 -- 7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.
-select * from product as p where quantity < 10;
+select t.name, sum(p.quantity) from type as t join product as p on p.type_id = t.id group by t.name, p.type_id, t.id having sum(p.quantity) > 10;
 
 -- 8. Вывести все продукты и их тип.
 select p.name, t.name from product as p
