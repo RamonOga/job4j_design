be32ade3263824620b224132251d77d70eba7381
Roman Markelov <Roman.sercent@gmail.com>
2021-03-29 11:02:30 +0300

    1. Принципы Kiss, Dry и Yagni [#407787]

---
+++ chapter_005/src/main/java/ru/job4j/tdd/MaxMin.java
@@ -0,0 +1,28 @@
+package ru.job4j.tdd;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class MaxMin {
+    public <T> T max(List<T> value, Comparator<T> comparator) {
+        Predicate<Integer> maxPredicate = (a) -> (a == -1);
+        return comp(value, comparator, maxPredicate);
+    }
+
+    public <T> T min(List<T> value, Comparator<T> comparator) {
+        Predicate<Integer> minPredicate = (a) -> (a == 1);
+        return comp(value, comparator, minPredicate);
+    }
+
+    private <T> T comp(List<T> value, Comparator<T> comparator, Predicate<Integer> pred) {
+        T rsl = value.get(0);
+        for (int i = 1; i < value.size(); i++) {
+            T tmp = value.get(i);
+            if (pred.test(comparator.compare(rsl, tmp))) {
+                rsl = tmp;
+            }
+        }
+        return rsl;
+    }
+}
