e1852d0b74a935618a2b1adf68fb2c8068c90088
Roman Markelov <Roman.sercent@gmail.com>
2021-03-31 13:29:32 +0300

    SQL Select by company [#407702]

---
+++ chapter_003/src/main/java/ru/job4j/postgresql/scripts/lasttask/insert.sql
@@ -0,0 +1,16 @@
+insert into company (id, name) values (1, 'google');
+insert into company (id, name) values (2, 'oracle');
+insert into company (id, name) values (3, 'amazon');
+insert into company (id, name) values (4, 'microsoft');
+insert into company (id, name) values (5, 'yandex');
+insert into company (id, name) values (6, 'epam');
+
+insert into person (id, name, company_id) values(1, 'Petr', 1);
+insert into person (id, name, company_id) values(2, 'Vasia', 1);
+insert into person (id, name, company_id) values(3, 'Fedor', 2);
+insert into person (id, name, company_id) values(4, 'Roman', 2);
+insert into person (id, name, company_id) values(5, 'Grisha', 3);
+insert into person (id, name, company_id) values(6, 'Ruslan', 4);
+insert into person (id, name, company_id) values(7, 'Gena', 5);
+insert into person (id, name, company_id) values(8, 'Danila', 6);
+insert into person (id, name, company_id) values(9, 'Nikolai', 2);
---
+++ chapter_003/src/main/java/ru/job4j/postgresql/scripts/lasttask/queres.sql
@@ -0,0 +1,18 @@
+/*1. В одном запросе получить
+  - имена всех person, которые не состоят в компании с id = 5;
+  - название компании для каждого человека.*/
+select p.id, p.name, c.name  from person as p
+join company as c on p.company_id = c.id and c.id != 5;
+
+select c.name, count(p.company_id) from company as c
+join person as p on c.id = p.company_id
+group by c.name, p.company_id;
+
+--2. Необходимо выбрать название компании с максимальным количеством человек + количество человек в этой компании.
+
+-- Создаем представление:
+create view presoncount as (select c.name, count(p.company_id) from company as c
+join person as p on c.id = p.company_id
+group by c.name, p.company_id);
+-- И запрос
+select name, max(count) from personcount group by name having  max(count) = (select max(count) from personcount);
