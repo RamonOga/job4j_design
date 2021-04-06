0fc2a8bcde7e0173ff5c4f2b45af643a21825855
Ramon Markelov <Roman.sercent@gmail.com>
2021-04-06 20:42:10 +0300

    168. Excel Sheet Column Title

--- chapter_005/src/main/java/ru/job4j/TestRunner.java
+++ chapter_005/src/main/java/ru/job4j/TestRunner.java
@@ -1,16 +1,32 @@
 package ru.job4j;
 
-public class TestRunner implements Cloneable {
-    String name;
-    int age;
+public class TestRunner {
+    public static void main(String[] args) {
+        System.out.println(convertToTitle(701));
+        System.out.println(convertToTitle(676));
+        System.out.println(convertToTitle(28));
+        System.out.println(convertToTitle(2147483647));
+        System.out.println(convertToTitle(1));
 
-    public TestRunner(String name, int age) {
-        this.name = name;
-        this.age = age;
+        }
+
+    public static String convertToTitle(int columnNumber) {
+        String rsl = "";
+        while (columnNumber / 26 > 0) {
+            rsl += getWord(columnNumber % 26);
+            columnNumber /= 26;
+        }
+        rsl += getWord(columnNumber % 26);
+        return rsl;
     }
 
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
+    private static String getWord(int columnNumber) {
+        if (columnNumber == 0) {
+            return "";
+        }
+        final int upLimit = 64;
+        char c = (char) (upLimit + columnNumber);
+        String rsl = String.valueOf(c);
+        return rsl;
     }
 }
