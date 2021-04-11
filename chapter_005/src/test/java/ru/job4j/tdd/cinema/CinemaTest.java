9b3f06b7605abe0b8892bb1f2a4d5e0e37994481
Ramon Markelov <Roman.sercent@gmail.com>
2021-04-11 11:45:59 +0300

    2. Что такое TDD? [#407789]

--- chapter_005/src/test/java/ru/job4j/tdd/cinema/CinemaTest.java
+++ chapter_005/src/test/java/ru/job4j/tdd/cinema/CinemaTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -18,9 +19,9 @@ public class CinemaTest {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
-        date.set(2020, 10, 10, 23, 00);
+        date.set(2021, Calendar.NOVEMBER, 10, 23, 0);
         Ticket ticket = cinema.buy(account, 1, 1, date);
-        assertThat(ticket, is(new Ticket3D()));
+        assertEquals(ticket, is(new Ticket3D()));
     }
 
     @Test
@@ -28,7 +29,7 @@ public class CinemaTest {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
-        assertThat(sessions, is(Arrays.asList(new Session3D())));
+        assertEquals(sessions, is(Collections.singletonList(new Session3D())));
     }
 
     @Test
@@ -37,5 +38,32 @@ public class CinemaTest {
         Session session = new Session3D();
         cinema.add(session);
         assertEquals(session, cinema.find(a -> true).get(0));
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void buyTwoTicketsOnOnePlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, Calendar.NOVEMBER, 10, 23, 0);
+        Ticket ticket1 = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void buyWithWrongDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2000, Calendar.NOVEMBER, 10, 23, 0);
+        Ticket ticket1 = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void buyWithWrongPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, Calendar.NOVEMBER, 10, 23, 0);
+        Ticket ticket1 = cinema.buy(account, 1000, 1000, date);
     }
 }
