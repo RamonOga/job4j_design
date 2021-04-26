ae09dd09e62f30c9bfb19938b2a1f80b8a63dbe0
Roman Markelov <Roman.sercent@gmail.com>
2021-04-26 08:13:50 +0300

    trash commit

---
+++ chapter_003/src/main/java/ru/job4j/leetcode/ReverseLinkList.java
@@ -0,0 +1,28 @@
+package ru.job4j.leetcode;
+
+
+
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+
+public class ReverseLinkList {
+
+        public ListNode reverseList(ListNode head) {
+            ListNode one = head;
+            ListNode two = head.next;
+            ListNode three;
+            while(one.next != null) {
+                three = one;
+                one = two;
+                two = one.next;
+                one.next = three;
+            }
+            return two;
+        }
+}
--- chapter_004/src/main/java/ru/job4j/jmh/Runner.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.jmh;
-
-
-import java.util.*;
-
-public class Runner {
-    public static void main(String[] args) throws Exception {
-    }
-}
