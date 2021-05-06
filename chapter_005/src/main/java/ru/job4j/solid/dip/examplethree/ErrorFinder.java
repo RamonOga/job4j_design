2fbe1c1867689e9df5dd7ed3e5b60a85d4671691
Ramon Markelov <Roman.sercent@gmail.com>
2021-05-06 15:09:53 +0300

    SOLID. Dependency Inversion Principe. Example #3

--- chapter_005/src/main/java/ru/job4j/solid/dip/examplethree/ErrorFinder.java
+++ chapter_005/src/main/java/ru/job4j/solid/dip/examplethree/ErrorFinder.java
@@ -4,17 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ErrorFinder {
-    List<String> errorList;
-
-    public ErrorFinder() {
-        this.errorList = new ArrayList<>();
-    }
 
     public void errorFind(DataInfo info) {
         List<String> list = info.getDataList();
         for (String s : list) {
             if (s.toLowerCase().contains("error")) {
-                errorList.add(s);
+                System.out.println(s);
             }
         }
     }
