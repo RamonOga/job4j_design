31b4546d70926bab7bbb25d5f5319559eebaac8a
Roman Markelov <Roman.sercent@gmail.com>
2021-05-06 11:00:46 +0300

    Создать меню. убрал лишнее.

--- chapter_005/src/main/java/ru/job4j/solid/isp/menu/MenuNode.java
+++ chapter_005/src/main/java/ru/job4j/solid/isp/menu/MenuNode.java
@@ -1,21 +1,11 @@
 package ru.job4j.solid.isp.menu;
 
-import java.awt.*;
 import java.util.List;
 
 public class MenuNode {
 
     private String menuName;
-
-    public List<MenuNode> getChildren() {
-        return children;
-    }
-
     private List<MenuNode> children;
-
-    public String getMenuName() {
-        return menuName;
-    }
 
     public MenuNode(String menuName, List<MenuNode> childrens) {
         this.menuName = menuName;
@@ -24,19 +14,6 @@ public class MenuNode {
 
     public MenuNode(String menuName) {
         this.menuName = menuName;
-    }
-
-    public void printMenu() {
-        System.out.println("menu name: " + menuName);
-        System.out.println(children);
-    }
-
-    private String getChildrenNames() {
-        String rsl = "";
-        for (MenuNode menuNode : children) {
-
-        }
-        return rsl;
     }
 
     @Override
