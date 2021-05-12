1ab44df193ec15037b1a9090ee8d1d8411a59b94
Roman Markelov <Roman.sercent@gmail.com>
2021-05-12 09:06:07 +0300

    SOLID. Dependency Inversion Principle. Example #2

--- chapter_005/src/main/java/ru/job4j/solid/dip/exampletwo/CarConstructor.java
+++ chapter_005/src/main/java/ru/job4j/solid/dip/exampletwo/CarConstructor.java
@@ -1,8 +1,28 @@
 package ru.job4j.solid.dip.exampletwo;
 
-public class CarConstructor {
+import java.util.List;
+import java.util.function.Predicate;
 
-    public Car carConstruct(Engine engine, Body body, Transmission transmission) {
-        return new Car(engine, body, transmission);
+public class CarConstructor {
+    List<Body> bodies;
+    List<Engine> engines;
+    List<Transmission> transmissions;
+
+    public Car carConstruct(Predicate<Body> bodyPredicate,
+                            Predicate<Engine> enginePredicate,
+                            Predicate<Transmission> transmissionPredicate) {
+        return new Car(findEngine(enginePredicate),
+                findBody(bodyPredicate),
+                findTransmission(transmissionPredicate));
+    }
+
+    private Body findBody(Predicate<Body> predicate) {
+        return bodies.stream().filter(predicate).findFirst().get();
+    }
+    private Engine findEngine(Predicate<Engine> predicate) {
+        return engines.stream().filter(predicate).findFirst().get();
+    }
+    private Transmission findTransmission(Predicate<Transmission> predicate) {
+        return transmissions.stream().filter(predicate).findFirst().get();
     }
 }
