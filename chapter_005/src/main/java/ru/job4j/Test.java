813d6ea9bfa98bcd297c80925d282b54a0c78479
Roman Markelov <Roman.sercent@gmail.com>
2021-06-15 09:30:48 +0300

    test commit

--- chapter_005/src/main/java/ru/job4j/Test.java
+++ chapter_005/src/main/java/ru/job4j/Test.java
@@ -3,6 +3,10 @@ package ru.job4j;
 public class Test {
 
     public static void main(String[] args) {
+        hello();
+    }
+
+    public static void hello() {
         System.out.println("Hello world!!!");
     }
 
